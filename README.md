5278c4c719fd1852c6d442eff06362d77e77f9b2
Aleksey Smirnov <lirien42@gmail.com>
2018-08-05 20:36:47 +0300

    fix lint problems

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # examples (knowlage-base for myself in future)
 
-Travis build: 
+Travis build: [![Build Status](https://travis-ci.org/Poletay/examples.svg?branch=master)](https://travis-ci.org/Poletay/examples)
 
-CodeClimate: 
+CodeClimate: [![Maintainability](https://api.codeclimate.com/v1/badges/cc92cd672c7b927e00bf/maintainability)](https://codeclimate.com/github/Poletay/examples/maintainability)
--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,7 @@
-import _ from 'lodash';
-
-export default (router) => {
-  return router
-    .get('root', '/', (ctx) => {
-      ctx.render('index', { Title: 'Main page'});
-    })
-    .get('/users', (ctx) => {
-      ctx.render('index', { Title: 'Users'});
-    });
-}
+export default router => router
+  .get('root', '/', (ctx) => {
+    ctx.render('index', { Title: 'Main page' });
+  })
+  .get('/users', (ctx) => {
+    ctx.render('index', { Title: 'Users' });
+  });
