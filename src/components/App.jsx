ec8b25bf1a5e02c51542a9d8a192d3004a881f50
Aleksey Smirnov <lirien42@gmail.com>
2018-08-09 22:29:55 +0300

    add main grid fo layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
-const App = () => <div className="container">Some awesome text...</div>;
+const App = () => (
+  <div className="main">
+    <div className="row">
+      <div className="col-12">HEADER HERE</div>
+    </div>
+    <div className="row">
+      <div className="col-3">LEFT CONTENT BAR HERE</div>
+      <div className="col-7">CENTER CONTENT - MAIN CONTENT</div>
+      <div className="col-2">RIGHT CONTENT BAR HERE</div>
+    </div>
+    <div className="row">
+      <div className="col-12">FOOTER HERE</div>
+    </div>
+  </div>
+);
 
 export default App;
