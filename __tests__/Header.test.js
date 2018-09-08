d54bf2560ec19886fef31bf5d9c98ec72d0e5f41
Aleksey Smirnov <lirien42@gmail.com>
2018-09-08 15:53:20 +0300

    add tests for Header component

---
+++ __tests__/Header.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+import Header from '../src/components/Header';
+
+configure({
+  adapter: new Adapter(),
+});
+
+describe('App components test', () => {
+  it('should be selectable by class "main-header"', () => {
+    expect(shallow(<Header />).is('.main-header')).toBe(true);
+  });
+});
+
+describe('Main header layout tests', () => {
+  const component = shallow(<Header />);
+  it('Main header contain only 2 elements', () => {
+    expect(component.children().length).toBe(3);
+  });
+  it('first element should contain MainLogo component', () => {
+    const firstElement = component.childAt(0).childAt(0);
+    expect(firstElement.name()).toBe('MainLogo');
+  });
+  it('second element should contain MainMenu component', () => {
+    const secondElement = component.childAt(1).childAt(0);
+    expect(secondElement.name()).toBe('MainMenu');
+  });
+});
