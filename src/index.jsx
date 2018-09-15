6649cdcf478affd06c5a7e1212794a9717015bb5
Aleksey Smirnov <lirien42@gmail.com>
2018-09-15 19:28:50 +0300

    add some menu-items

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,20 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 // import { createStore, compose } from 'redux';
 // import { Provider } from 'react-redux';
 // import thunk from 'redux-thunk';
 // import reducers from './reducers';
 import App from './components/App';
+import Home from './components/contentChapters/Home';
 import AlgorithmsChapter from './components/contentChapters/AlgorithmsChapter';
 import LayoutsChapter from './components/contentChapters/LayoutsChapter';
 import LibsChapter from './components/contentChapters/LibsChapter';
-import Home from './components/contentChapters/Home';
+import PatternsChapter from './components/contentChapters/PatternsChapter';
+import PaymentChapter from './components/contentChapters/PaymentChapter';
+import SequrityChapter from './components/contentChapters/SequrityChapter';
+import SicpChapter from './components/contentChapters/SicpChapter';
 
 // import menuItems from './components/contentChapters';
 
@@ -34,31 +38,19 @@ const store = createStore(
 */
 
 const mainAppStructure = [
-  {
-    name: 'Home',
-    path: '/',
-    component: Home,
-  },
-  {
-    name: 'Algorithms',
-    path: '/algorithms',
-    component: AlgorithmsChapter,
-  },
-  {
-    name: 'Layouts',
-    path: '/layouts',
-    component: LayoutsChapter,
-  },
-  {
-    name: 'Libs',
-    path: '/libs',
-    component: LibsChapter,
-  },
+  { name: 'Home', path: '/', component: Home },
+  { name: 'Algorithms', path: '/algorithms', component: AlgorithmsChapter },
+  { name: 'Layouts', path: '/layouts', component: LayoutsChapter },
+  { name: 'Libs', path: '/libs', component: LibsChapter },
+  { name: 'Patterns', path: '/patterns', component: PatternsChapter },
+  { name: 'Payment', path: '/payment', component: PaymentChapter },
+  { name: 'Sequrity', path: '/sequrity', component: SequrityChapter },
+  { name: 'SICP', path: '/sicp', component: SicpChapter },
 ];
 
 render(
-  <BrowserRouter>
+  <HashRouter>
     <App mainAppStructure={mainAppStructure} />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('container'),
 );
