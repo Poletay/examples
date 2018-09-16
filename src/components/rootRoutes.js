e5ac676b9793fb99cf7da1f1ce7d2649aa6d8433
Aleksey Smirnov <lirien42@gmail.com>
2018-09-16 20:25:10 +0300

    change share for MainMenu routes from props to context

--- src/components/rootRoutes.js
+++ src/components/rootRoutes.js
@@ -1,24 +1,5 @@
 import React from 'react';
-import Home from './contentChapters/Home';
-import AlgorithmsChapter from './contentChapters/AlgorithmsChapter';
-import LayoutsChapter from './contentChapters/LayoutsChapter';
-import LibsChapter from './contentChapters/LibsChapter';
-import PatternsChapter from './contentChapters/PatternsChapter';
-import PaymentChapter from './contentChapters/PaymentChapter';
-import SequrityChapter from './contentChapters/SequrityChapter';
-import SicpChapter from './contentChapters/SicpChapter';
-import AboutMe from './contentChapters/AboutMe';
 
-const rootRoutes = React.createContext([
-  { name: 'Home', path: '/', component: Home },
-  { name: 'Algorithms', path: '/algorithms', component: AlgorithmsChapter },
-  { name: 'Layouts', path: '/layouts', component: LayoutsChapter },
-  { name: 'Libs', path: '/libs', component: LibsChapter },
-  { name: 'Patterns', path: '/patterns', component: PatternsChapter },
-  { name: 'Payment', path: '/payment', component: PaymentChapter },
-  { name: 'Sequrity', path: '/sequrity', component: SequrityChapter },
-  { name: 'SICP', path: '/sicp', component: SicpChapter },
-  { name: 'About me', path: '/aboutme', component: AboutMe },
-]);
+const rootRoutes = React.createContext([]);
 
 export default rootRoutes;
