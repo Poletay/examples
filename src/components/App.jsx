9a02c36d2d43a478c1771a1f4789b4352e9dd808
Aleksey Smirnov <lirien42@gmail.com>
2018-09-16 20:27:52 +0300

    try to fix codeclimate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Header from './Header';
 import Middle from './Middle';
 import Footer from './Footer';
 
-class App extends React.Component {
-  static propTypes = {
-    mainAppStructure: PropTypes.array,
-  }
-
-  render() {
-    const { mainAppStructure } = this.props;
-    return (
-      <div className="main">
-        <div className="row">
-          <div className="col-12">
-            <Header mainAppStructure={mainAppStructure} />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12">
-            <Middle contents={mainAppStructure} />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12">
-            <Footer />
-          </div>
-        </div>
+const App = () => (
+  <div className="main">
+    <div className="row">
+      <div className="col-12">
+        <Header />
       </div>
-    );
-  }
-}
-
-App.PropTypes = {
-  mainAppStructure: PropTypes.array,
-};
+    </div>
+    <div className="row">
+      <div className="col-12">
+        <Middle />
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-12">
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
