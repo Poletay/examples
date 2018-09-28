ec880ee8d2a87b24b6badfc47ee4e1f23fd3fb78
Aleksey Smirnov <lirien42@gmail.com>
2018-09-29 02:43:42 +0300

    try to fix codeclimate issues

--- src/components/contentChapters/AlgorithmsChapter/SortAlgorithms/index.jsx
+++ src/components/contentChapters/AlgorithmsChapter/SortAlgorithms/index.jsx
@@ -3,23 +3,21 @@ import PropTypes from 'prop-types';
 import Menu from '../../../Menu';
 import RoutesList from '../../../RoutesList';
 
-export default class SortAlgorithms extends React.Component {
-  static propTypes = {
-    routes: PropTypes.array,
-  }
-
-  render() {
-    return (
-      <div className="chapter-algorithms">
-        <div className="row">
-          <div className="col-3">
-            <Menu routes={this.props.routes} menuName='right-side-menu' menuType='flex-column' />
-          </div>
-          <div className="col-9">
-            <RoutesList routes={this.props.routes} />
-          </div>
-        </div>
+const SortAlgorithms = ({ routes }) => (
+  <div className="chapter-algorithms">
+    <div className="row">
+      <div className="col-3">
+        <Menu routes={routes} menuName='right-side-menu' menuType='flex-column' />
       </div>
-    );
-  }
-}
+      <div className="col-9">
+        <RoutesList routes={routes} />
+      </div>
+    </div>
+  </div>
+);
+
+SortAlgorithms.propTypes = {
+  routes: PropTypes.array,
+};
+
+export default SortAlgorithms;
