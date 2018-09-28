9fb11f36646c88c6ba2bdae370eae31e2ab21753
Aleksey Smirnov <lirien42@gmail.com>
2018-09-29 02:47:06 +0300

    try to fix codeclimate issues

--- src/components/contentChapters/AlgorithmsChapter/index.jsx
+++ src/components/contentChapters/AlgorithmsChapter/index.jsx
@@ -3,23 +3,21 @@ import PropTypes from 'prop-types';
 import Menu from '../../Menu';
 import RoutesList from '../../RoutesList';
 
-export default class AlgorithmsChapter extends React.Component {
-  static propTypes = {
-    routes: PropTypes.array,
-  }
-
-  render() {
-    return (
-      <div className="chapter-algorithms">
-        <div className="row">
-          <div className="col-3">
-            <Menu routes={this.props.routes} menuName='left-side-menu' menuType='flex-column' />
-          </div>
-          <div className="col-9">
-            <RoutesList routes={this.props.routes} />
-          </div>
-        </div>
+const AlgorithmsChapter = ({ routes }) => (
+  <div className="chapter-algorithms">
+    <div className="row">
+      <div className="col-3">
+        <Menu routes={routes} menuName='left-side-menu' menuType='flex-column' />
       </div>
-    );
-  }
-}
+      <div className="col-9">
+        <RoutesList routes={routes} />
+      </div>
+    </div>
+  </div>
+);
+
+AlgorithmsChapter.propTypes = {
+  routes: PropTypes.array,
+};
+
+export default AlgorithmsChapter;
