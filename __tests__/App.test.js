4eee4fe3274a3fa8d60d8dec71c2cfcc999fde8b
Aleksey Smirnov <lirien42@gmail.com>
2018-10-06 09:45:12 +0300

    add new tests for App component (main Layout)

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -2,38 +2,30 @@ import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
 import App from '../src/components/App';
+import Header from '../src/components/Header';
+import Middle from '../src/components/Middle';
+import Footer from '../src/components/Footer';
 
 configure({
   adapter: new Adapter(),
 });
 
 describe('App component test', () => {
+  const component = shallow(<App />);
   it('should be selectable by class "main"', () => {
     expect(shallow(<App />).is('.main')).toBe(true);
   });
-});
 
-/*
-describe('Main layout tests', () => {
-  const component = shallow(<App />);
-  it('should contain 3 rows"', () => {
-    expect(component.childAt(0).childAt(0).children().length).toBe(3);
-    expect(component.children('div.row').length).toBe(3);
-  });
+  describe('Main layout tests', () => {
+    it('should contain 3 rows"', () => {
+      expect(component.find('div.row').length).toBe(3);
+    });
 
-  it('first row should contain Header component', () => {
-    const headerCell = component.childAt(0).childAt(0).childAt(0);
-    expect(headerCell.name()).toBe('Header');
-  });
-
-  it('middle row should contain Middle component', () => {
-    const middleCell = component.childAt(1).childAt(0).childAt(0);
-    expect(middleCell.name()).toBe('Middle');
-  });
-
-  it('last row should contain Footer component', () => {
-    const footerCell = component.childAt(2).childAt(0).childAt(0);
-    expect(footerCell.name()).toBe('Footer');
+    it('first row should contain Header, Middle and Footer components', () => {
+      console.log(component.find('div.row').at(0).debug());
+      expect(component.contains(<Header />)).toBe(true);
+      expect(component.contains(<Middle />)).toBe(true);
+      expect(component.contains(<Footer />)).toBe(true);
+    });
   });
 });
-*/
