b8ff2d6a5e02b23e2e9aebb8322b532291bcb85d
Aleksey Smirnov <lirien42@gmail.com>
2018-10-23 22:22:17 +0300

    refactoring for bubble sorting - try to fix codeclimate issues

--- src/components/contentChapters/AlgorithmsChapter/SortAlgorithms/BubbleSort.jsx
+++ src/components/contentChapters/AlgorithmsChapter/SortAlgorithms/BubbleSort.jsx
@@ -6,42 +6,31 @@ const algorithmName = 'bubbleSorting';
 const description = 'Bubble sorting is a simple sorting algorithm that repeatedly steps through the list to be sorted, compares each pair of adjacent items and swaps them if they are in the wrong order. The pass through the list is repeated until no swaps are needed, which indicates that the list is sorted.';
 const entryData = [5, 2, 1, 8, 4, 7, 3, 6];
 
-const sortArray = (arr) => {
-  const startIndex = 0;
-  const bubbleSort = ({ sortingArray, currentIndex, status }) => {
-    if (status === 'notchanged' && currentIndex === arr.length) {
-      return sortingArray;
-    }
+const bubbleSort = (sortingArray, currentIndex = 0, status = 'notchanged') => {
+  if (status === 'notchanged' && currentIndex === sortingArray.length) {
+    return sortingArray;
+  }
 
-    if (status === 'changed' && currentIndex === sortingArray.length) {
-      return sortArray(sortingArray);
-    }
+  if (status === 'changed' && currentIndex === sortingArray.length) {
+    return bubbleSort(sortingArray);
+  }
 
-    const nextIndex = currentIndex + 1;
-    const intermidiateResult = {
-      sortingArray: [...sortingArray],
-      currentIndex: nextIndex,
-      status,
-    };
+  const nextIndex = currentIndex + 1;
+  const resultArray = [...sortingArray];
+  let currentStatus = status;
 
-    if (intermidiateResult.sortingArray[currentIndex]
-      > intermidiateResult.sortingArray[nextIndex]) {
-      [
-        intermidiateResult.sortingArray[currentIndex],
-        intermidiateResult.sortingArray[nextIndex],
-      ] = [
-        intermidiateResult.sortingArray[nextIndex],
-        intermidiateResult.sortingArray[currentIndex],
-      ];
-      intermidiateResult.status = 'changed';
-    }
-    return bubbleSort(intermidiateResult);
-  };
-  return bubbleSort({
-    sortingArray: arr,
-    currentIndex: startIndex,
-    status: 'notchanged',
-  });
+  if (resultArray[currentIndex]
+    > resultArray[nextIndex]) {
+    [
+      resultArray[currentIndex],
+      resultArray[nextIndex],
+    ] = [
+      resultArray[nextIndex],
+      resultArray[currentIndex],
+    ];
+    currentStatus = 'changed';
+  }
+  return bubbleSort(resultArray, nextIndex, currentStatus);
 };
 
 const algorithm = {
@@ -49,7 +38,7 @@ const algorithm = {
   name: algorithmName,
   description,
   entryData,
-  func: sortArray,
+  func: bubbleSort,
 };
 
 const BubbleSort = () => <AlgorithmsLayoutTeamplate algorithm={algorithm} />;
