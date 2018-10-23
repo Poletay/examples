3d93ca7e7a356051e78ac0775a268b30d9475c00
Aleksey Smirnov <lirien42@gmail.com>
2018-10-23 22:42:26 +0300

    refactoring for bubble sorting - try to fix codeclimate issues

--- src/components/contentChapters/AlgorithmsChapter/SortAlgorithms/BubbleSort.jsx
+++ src/components/contentChapters/AlgorithmsChapter/SortAlgorithms/BubbleSort.jsx
@@ -7,14 +7,14 @@ const description = 'Bubble sorting is a simple sorting algorithm that repeatedl
 const entryData = [5, 2, 1, 8, 4, 7, 3, 6];
 
 const bubbleSort = (sortingArray, currentIndex = 0, status = 'notchanged') => {
-  const isSortingComplete = status === 'notchanged' && currentIndex === sortingArray.length;
-  const isSortingIterationComplete = status === 'changed' && currentIndex === sortingArray.length;
+  const isSortingComplete = status === 'notchanged';
+  const isSortingIterationComplete = currentIndex === sortingArray.length;
 
-  if (isSortingComplete) {
+  if (isSortingComplete && isSortingIterationComplete) {
     return sortingArray;
   }
 
-  if (isSortingIterationComplete) {
+  if (!isSortingComplete && isSortingIterationComplete) {
     return bubbleSort(sortingArray);
   }
 
