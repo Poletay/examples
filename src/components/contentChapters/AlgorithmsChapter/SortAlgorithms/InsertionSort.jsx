b32f6958d36851093120d3311526c30b397269b8
Aleksey Smirnov <lirien42@gmail.com>
2018-10-26 09:10:34 +0300

    add insertion sort implementation

--- src/components/contentChapters/AlgorithmsChapter/SortAlgorithms/InsertionSort.jsx
+++ src/components/contentChapters/AlgorithmsChapter/SortAlgorithms/InsertionSort.jsx
@@ -4,11 +4,29 @@ import AlgorithmsLayoutTeamplate from '../AlgorithmsLayoutTemplate';
 const algorithmTitle = 'Insertion sorting';
 const algorithmName = 'insertionSorting';
 const description = 'Insertion sort iterates, consuming one input element each repetition, and growing a sorted output list. At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list, and inserts it there. It repeats until no input elements remain.';
-const entryData = [9, 5, 2, 1, 8, 4, -2, 7, 3, 6, 0, -1];
+const entryData = [9, 5, 2, 1, 8, 4, 7, 3, 6, 0, -1];
 
-const insertionSort = sortingArray => sortingArray;
+const insertionSort = (sortingArray, currentIndex, sortingElement) => {
+  const resultArray = [...sortingArray];
+  const nextIndex = currentIndex - 1;
+  if (currentIndex === 0 || sortingElement > resultArray[nextIndex]) {
+    resultArray[currentIndex] = sortingElement;
+    return resultArray;
+  }
 
-const sortArray = arr => insertionSort(arr);
+  resultArray[currentIndex] = resultArray[nextIndex];
+  return insertionSort(resultArray, nextIndex, sortingElement);
+};
+
+const sortArray = (arr, currentIndex = 0) => {
+  if (arr.length < 2 || currentIndex === arr.length) {
+    return arr;
+  }
+
+  const resultArray = insertionSort(arr, currentIndex, arr[currentIndex]);
+  const nextIndex = currentIndex + 1;
+  return sortArray(resultArray, nextIndex);
+};
 
 const algorithm = {
   title: algorithmTitle,
