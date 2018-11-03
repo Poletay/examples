11fd73f3055bdcc62e84e2e02980f2eee964924f
Aleksey Smirnov <lirien42@gmail.com>
2018-11-03 12:58:48 +0300

    remove dated components

--- src/components/lazyLoad.js
+++
@@ -1,7 +0,0 @@
-import Loadable from 'react-loadable';
-import Loading from './Loading';
-
-export default componentPath => Loadable({
-  loader: () => import(componentPath),
-  loading: Loading,
-});
