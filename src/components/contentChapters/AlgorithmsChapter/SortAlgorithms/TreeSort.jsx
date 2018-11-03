98445d8e746b8f2207a75f90e1eef9da3c29218c
Aleksey Smirnov <lirien42@gmail.com>
2018-11-03 12:54:50 +0300

    add tree sorting algorithm implementation

--- src/components/contentChapters/AlgorithmsChapter/SortAlgorithms/TreeSort.jsx
+++ src/components/contentChapters/AlgorithmsChapter/SortAlgorithms/TreeSort.jsx
@@ -4,16 +4,49 @@ import AlgorithmsLayoutTeamplate from '../AlgorithmsLayoutTemplate';
 const algorithmTitle = 'Tree sorting';
 const algorithmName = 'treeSorting';
 const description = 'A tree sort is a sort algorithm that builds a binary search tree from the elements to be sorted, and then traverses the tree (in-order) so that the elements come out in sorted order.';
-const entryData = [9, 5, 2, 1, 8, 4, 7, 3, 6, 0, -1];
+const entryData = [5, 2, 1, 8, 4, 9, 7, 3, 6, 0, -1, 5];
 
-const treeSort = (sortingArray) => {
-  const resultArray = [...sortingArray];
+const createBinarySortingTree = (arr) => {
+  const createNode = () => ({
+    value: null,
+    left: null,
+    right: null,
+  });
+
+  const addTreeElement = (tree, element) => {
+    if (tree.value === null) {
+      return ({
+        ...tree,
+        value: element,
+        left: createNode(),
+        right: createNode(),
+      });
+    }
+
+    const nextChildren = element < tree.value ? 'left' : 'right';
+    return {
+      ...tree,
+      [nextChildren]: addTreeElement(tree[nextChildren], element),
+    };
+  };
+
+  return arr.reduce((acc, item) => addTreeElement(acc, item), createNode());
+};
+
+const treeToArray = (tree) => {
+  const resultArray = tree.value === null ? [] : [
+    ...treeToArray(tree.left),
+    tree.value,
+    ...treeToArray(tree.right),
+  ];
   return resultArray;
 };
 
 const sortArray = (arr) => {
-  const resultArray = treeSort(arr);
-  return resultArray;
+  const sortingTree = createBinarySortingTree(arr);
+  const sortedArray = treeToArray(sortingTree);
+
+  return sortedArray;
 };
 
 const algorithm = {
