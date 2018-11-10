f06d8ce68e3867a2abc1363d81a22818e3c93c03
Aleksey Smirnov <lirien42@gmail.com>
2018-11-10 17:45:23 +0300

    try to fix codeclimate issues

--- src/components/contentChapters/AlgorithmsChapter/AlgorithmsTemplate.jsx
+++ src/components/contentChapters/AlgorithmsChapter/AlgorithmsTemplate.jsx
@@ -4,6 +4,8 @@ import connect from '../../../connect';
 
 const mapStateToProps = ({ algorithms }) => ({ algorithmsState: { ...algorithms } });
 
+const prettifyData = data => <pre>{JSON.stringify(data, null, '\t')}</pre>;
+
 class AlgorithmsLayoutTemplate extends React.Component {
   onApplyAlgorithmButtonClick = () => {
     const { func, name, entryData } = this.props.algorithm;
@@ -34,10 +36,10 @@ class AlgorithmsLayoutTemplate extends React.Component {
           <h5 className="card-title">{title}</h5>
         </div>
         <div className="card-body">{description}</div>
-        <div className="card-body"><strong>Entry data:</strong> <pre>{JSON.stringify(entryData, null, '\t')}</pre></div>
+        <div className="card-body"><strong>Entry data:</strong> {prettifyData(entryData)}</div>
         <div className="card-body">{entryDescription}</div>
         <div className="card-body"><button onClick={this.onApplyAlgorithmButtonClick}>Apply algorithm</button></div>
-        <div className="card-body"><strong>Result data:</strong>  <pre>{JSON.stringify(resultData, null, '\t')}</pre></div>
+        <div className="card-body"><strong>Result data:</strong>  {prettifyData(resultData)}</div>
       </div>
     );
   }
